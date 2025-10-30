b1f1672425b51d47aba32552e68d90ee4889f71e
MartinHZ <mzacha22@student.aau.dk>
2025-10-31 00:26:17 +0100

    Add Addition class usage in Main and update test reports

--- src/main/java/org/martin/testing/Main.java
+++ src/main/java/org/martin/testing/Main.java
@@ -3,6 +3,12 @@ package org.martin.testing;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej!");
+
+        Addition addition = new Addition();
+        int result = addition.add(3, 6);
+
+        System.out.println("3 + 6 = " + result);
+
     }
     
 }
